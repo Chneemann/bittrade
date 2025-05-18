d8fc5aba2753d37c6012a5d2e0bf9057814b9b2d
Chneemann <andre-kempf@mailbox.org>
2025-05-18 20:29:32 +0200

    style: add _form.scss and integrate into LoginComponent; cleanup SCSS in other components

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  private createLoginForm(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', Validators.required],
+      keepLoggedIn: [false],
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    if (this.form.invalid) return;
+    console.log(this.form.value);
+  }
 }
