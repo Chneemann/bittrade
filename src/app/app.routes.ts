589128e6299ff3f9641a10f41918d330f6742652
Chneemann <andre-kempf@mailbox.org>
2025-05-18 10:49:20 +0200

    feat: create AuthWrapperComponent and LoginComponent, add routing for authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthWrapperComponent } from './components/auth/auth-wrapper.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: 'auth',
+    component: AuthWrapperComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+];
