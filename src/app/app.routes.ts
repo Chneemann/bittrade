3e6aaba9841b6e2dc97c5f6ed093f1f5de96f786
Chneemann <andre-kempf@mailbox.org>
2025-05-18 16:32:33 +0200

    feat: add PrivacyPolicyComponent and LegalNoticeComponent, and include them in routing and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AuthWrapperComponent } from './components/auth/auth-wrapper.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -9,6 +11,9 @@ export const routes: Routes = [
     component: AuthWrapperComponent,
     children: [
       { path: 'login', component: LoginComponent },
+      { path: 'login/privacy-policy', component: PrivacyPolicyComponent },
+      { path: 'login/legal-notice', component: LegalNoticeComponent },
+
       { path: '', redirectTo: 'login', pathMatch: 'full' },
     ],
   },
