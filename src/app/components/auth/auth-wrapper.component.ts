2f0e2deabd761870abbdb2eef5cb565677185eb8
Chneemann <andre-kempf@mailbox.org>
2025-05-24 15:09:41 +0200

    feat: create, design and integrate AuthHeaderComponent in AuthWrapperComponent

--- src/app/components/auth/auth-wrapper.component.ts
+++ src/app/components/auth/auth-wrapper.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthHeaderComponent } from './header/auth-header.component';
 import { AuthFooterComponent } from './footer/auth-footer.component';
 
 @Component({
   selector: 'app-auth-wrapper',
-  imports: [RouterOutlet, AuthFooterComponent],
+  imports: [RouterOutlet, AuthHeaderComponent, AuthFooterComponent],
   templateUrl: './auth-wrapper.component.html',
   styleUrl: './auth-wrapper.component.scss',
 })
