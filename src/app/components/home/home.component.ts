69f775e7ec2e288bbd5ebed2f1ab574b1f85ebe1
Chneemann <andre-kempf@mailbox.org>
2025-05-28 13:17:40 +0200

    feat: add FooterComponent with CoinGecko attribution, legal notice, and privacy policy links

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
