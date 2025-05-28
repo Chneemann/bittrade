23c4b48731ebc098266d09f6dfc145131dcd9193
Chneemann <andre-kempf@mailbox.org>
2025-05-28 11:22:58 +0200

    feat: implement NavigationComponent and restructure HomeComponent as application wrapper

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
