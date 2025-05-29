2392c76c19a19d8dd89b36fbc2b67d9bdfb3a26c
Chneemann <andre-kempf@mailbox.org>
2025-05-29 18:45:50 +0200

    feat: create HeaderComponent and integrate into HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../shared/components/navigation/navigation.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, NavigationComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
