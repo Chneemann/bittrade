ba3ac8cc65df785aa18000a5bded3c562de45aed
Chneemann <andre-kempf@mailbox.org>
2025-05-29 17:30:00 +0200

    feat: highlight active navigation link using Angular's routerLinkActive directive

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit, OnDestroy {
+  currentPath: string = '';
+
+  private destroy$ = new Subject<void>();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.getCurrentPath();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  getCurrentPath(): void {
+    this.currentPath = this.router.url.substring(1);
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => {
+        this.currentPath = this.router.url.substring(1);
+      });
+  }
+
   reloadPage(): void {
     window.location.reload();
   }
