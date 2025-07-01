51061a390fa395c135e0ec7f6abe08e1b5ba5a1c
Chneemann <andre-kempf@mailbox.org>
2025-07-01 08:55:56 +0200

    feat: load user profile data via UserService with error handling

--- src/app/home/components/profile/profile.component.ts
+++ src/app/home/components/profile/profile.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PrimaryButtonComponent } from '../../../shared/components/buttons/primary-button/primary-button.component';
 import { RouterLink } from '@angular/router';
+import { UserProfile, UserService } from '../../services/user.service';
+import { catchError, Observable, of, Subject, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [RouterLink, PrimaryButtonComponent],
+  imports: [CommonModule, RouterLink, PrimaryButtonComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit, OnDestroy {
+  profile$!: Observable<UserProfile>;
+  private destroy$ = new Subject<void>();
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.profile$ = this.userService.getProfile().pipe(
+      takeUntil(this.destroy$),
+      catchError((error) => {
+        console.error('Profile could not be loaded', error);
+        return of({
+          id: '',
+          username: '',
+          email: '',
+          coin_purchases: 0,
+          coin_sales: 0,
+          held_coins: 0,
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
