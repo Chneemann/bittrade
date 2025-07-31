eaa8a7cc03b3742649ed019aca31e9e09bc49d94
Chneemann <andre-kempf@mailbox.org>
2025-07-31 07:22:50 +0200

    fix: suppress strokeDashoffset animation warning & coin-card-chart height.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
+// Suppresses the ngx-charts warning because of ‘strokeDashoffset’.
+const originalWarn = console.warn;
+console.warn = function (...args: any[]) {
+  const warningMessage = args[0];
+  if (
+    typeof warningMessage === 'string' &&
+    /strokeDashoffset/i.test(warningMessage)
+  ) {
+    return;
+  }
+  originalWarn.apply(console, args);
+};
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
