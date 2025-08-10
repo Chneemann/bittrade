c345d03b8194c2074353cb07f6a59e85206754f1
Chneemann <andre-kempf@mailbox.org>
2025-08-10 08:42:54 +0200

    feat: fetch and display coin transaction data

--- src/app/home/components/history/history.component.ts
+++ src/app/home/components/history/history.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { CoinTransactionService } from '../../services/coin-transactions.service';
+import { TransactionCardComponent } from '../transactions/transaction-card/transaction-card.component';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [TransactionCardComponent],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+  styleUrl: './history.component.scss',
 })
 export class HistoryComponent {
+  allTransactions: any[] = [];
+  dataLoaded = false;
 
+  constructor(
+    private coinTransactionService: CoinTransactionService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAllTransactions();
+  }
+
+  loadAllTransactions(): void {
+    this.coinTransactionService.getAllTransactions().subscribe((txs) => {
+      this.allTransactions = txs;
+      this.dataLoaded = true;
+      this.cdr.detectChanges();
+    });
+  }
 }
