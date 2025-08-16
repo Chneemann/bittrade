6a688300bf58f843b2694e8b7cf2483ca2e01973
Chneemann <andre-kempf@mailbox.org>
2025-08-16 07:07:10 +0200

    feat: merge transactions to enable sorted display

--- src/app/home/components/history/history.component.ts
+++ src/app/home/components/history/history.component.ts
@@ -5,16 +5,22 @@ import { WalletService } from '../../services/wallet.service';
 import { catchError, forkJoin, of } from 'rxjs';
 import { CoinTransaction } from '../../models/coin.model';
 import { WalletTransaction } from '../../models/wallet.model';
+import { FiatCardComponent } from '../transactions/fiat-card/fiat-card.component';
+
+type MergedTransaction =
+  | (CoinTransaction & { type: 'coin' })
+  | (WalletTransaction & { type: 'fiat' });
 
 @Component({
   selector: 'app-history',
-  imports: [CoinCardComponent],
+  imports: [CoinCardComponent, FiatCardComponent],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
 })
 export class HistoryComponent {
   allCoinTransactions: CoinTransaction[] = [];
   allFiatTransactions: WalletTransaction[] = [];
+  mergedTransactions: MergedTransaction[] = [];
   dataLoaded = false;
 
   constructor(
@@ -27,6 +33,7 @@ export class HistoryComponent {
     this.loadAllTransactions();
   }
 
+  // Public methods
   loadAllTransactions(): void {
     forkJoin({
       coinTxs: this.coinTransactionService
@@ -38,10 +45,36 @@ export class HistoryComponent {
     }).subscribe(({ coinTxs, fiatTxs }) => {
       this.allCoinTransactions = coinTxs;
       this.allFiatTransactions = fiatTxs;
-      console.log(this.allCoinTransactions, this.allFiatTransactions);
+
+      this.mergedTransactions = this.mergeAndSortTransactions(coinTxs, fiatTxs);
 
       this.dataLoaded = true;
       this.cdr.detectChanges();
     });
+  }
+
+  // Public helper methods
+  isCoin(tx: MergedTransaction): tx is CoinTransaction & { type: 'coin' } {
+    return (tx as any).type === 'coin';
+  }
+
+  isFiat(tx: MergedTransaction): tx is WalletTransaction & { type: 'fiat' } {
+    return (tx as any).type === 'fiat';
+  }
+
+  // Private helper methods
+  private mergeAndSortTransactions(
+    coinTxs: CoinTransaction[],
+    fiatTxs: WalletTransaction[]
+  ): MergedTransaction[] {
+    return (
+      [
+        ...coinTxs.map((tx) => ({ ...tx, type: 'coin' as const })),
+        ...fiatTxs.map((tx) => ({ ...tx, type: 'fiat' as const })),
+      ] as MergedTransaction[]
+    ).sort(
+      (a, b) =>
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+    );
   }
 }
